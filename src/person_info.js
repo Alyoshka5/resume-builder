2bc8998b0b84b66325b7e5654048da16fbd04a78
Alyoshka <alyoshkamyshko@gmail.com>
2023-04-08 08:32:02 -0700

    Change and add more dummy data

--- src/person_info.js
+++ src/person_info.js
@@ -14,22 +14,34 @@ const education = {
 };
 
 const summary = `
-    I am a full stack developer with a background in hospitality and customer service. I am passionate about creating user-friendly applications and learning new technologies.
+Organized and dependable candidate successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals. Hardworking and passionate job seeker with strong organizational skills eager to secure entry-level [Job Title] position. Ready to help team achieve company goa
 `;
 
 const experience = [
-    {
-      position: 'Full Stack Developer',
-      company: 'Example Company',
-      location: 'Example, CA',
-      start: 'May 2021',
-      end: 'Present',
-      description: [
-        'Developed a full stack application using React, Node.js, Express, and PostgreSQL.',
-        'Implemented a RESTful API to allow users to create, read, update, and delete data.',
-        'Designed a responsive user interface using React Bootstrap.',
-      ]
-    }
+  {
+    position: 'Front End Developer',
+    company: 'Example Company',
+    location: 'Seattle, WA',
+    start: 'May 2020',
+    end: 'May 2021',
+    description: [
+      'Developed a full stack application using React, Node.js, Express, and PostgreSQL.',
+      'Implemented a RESTful API to allow users to create, read, update, and delete data.',
+      'Designed a responsive user interface using React Bootstrap.',
+    ]
+  },
+  {
+    position: 'Full Stack Developer',
+    company: 'Example Company',
+    location: 'Example, CA',
+    start: 'May 2021',
+    end: 'Present',
+    description: [
+      'Developed a full stack application using React, Node.js, Express, and PostgreSQL.',
+      'Implemented a RESTful API to allow users to create, read, update, and delete data.',
+      'Designed a responsive user interface using React Bootstrap.',
+    ]
+  }
 ]
 
 const personInfo = { name, contact, education, summary, experience };
