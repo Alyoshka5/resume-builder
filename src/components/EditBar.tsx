36f8b8ee053d5280d4cc04c671549b962a9548d2
Alyoshka <alyoshkamyshko@gmail.com>
2023-06-03 10:39:16 -0700

    Add description point when adding new job

--- src/components/EditBar.tsx
+++ src/components/EditBar.tsx
@@ -178,7 +178,7 @@ export default function EditBar({
                                     month: 'Present',
                                     year: '',
                                 },
-                                description: [],
+                                description: [''],
                             });
                             onExperienceChange(newExperience);
                         }}>+ Add job</button>
